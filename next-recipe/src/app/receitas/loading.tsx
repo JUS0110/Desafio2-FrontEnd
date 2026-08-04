50a3150e8a09b7ade312d16ff3f0c52900b7bc62
JUS0110 <juliaellen20203ju@gmail.com>
2026-08-04 18:25:28 -0300

    Alteração: resolvendo avisos de sintaxe react

--- next-recipe/src/app/receitas/loading.tsx
+++ next-recipe/src/app/receitas/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <main className="max-w-7xl mx-auto px-6 py-10">
 
-      <section className="flex flex-col items-center justify-center min-h-[400px]">
+      <section className="flex flex-col items-center justify-center min-h-100">
 
         <div className="w-16 h-16 border-4 border-orange-500 border-t-transparent rounded-full animate-spin">
         </div>
